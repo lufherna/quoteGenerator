bd6f7823dfc83349232bc37c5eafc599723b1f35
Luis Hernandez <rojoclifford@gmail.com>
2020-08-02 15:22:23 -0400

    Create script.js

---
+++ script.js
@@ -0,0 +1,63 @@
+const quoteContainer = document.getElementById('quote-container');
+const quoteText = document.getElementById('quote');
+const authorText = document.getElementById('author');
+const twitterBtn = document.getElementById('twitter');
+const newQuoteBtn = document.getElementById('new-quote');
+const loader = document.getElementById('loader');
+
+function showLoadingSpinner() {
+    loader.hidden = false;
+    quoteContainer.hidden = true;
+}
+
+function removeLoadingSpinner() {
+    if (!loader.hidden) {
+        quoteContainer.hidden = false;
+        loader.hidden = true;
+    }
+}
+
+async function getQuoteFromApi() {
+    showLoadingSpinner();
+    const proxyUrl = "https://secret-harbor-15114.herokuapp.com/"
+    const apiUrl = 'http://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=json';
+    try {
+        const response = await fetch(proxyUrl + apiUrl);
+        const data = await response.json();
+        // if author is blank add Unknown
+        if (data.quoteAuthor === '') {
+            authorText.innerText = 'Unknown';
+        } else {
+                authorText.innerText = data.quoteAuthor
+            }
+        // Reduce font size for long quotes
+        if (data.quoteText.length > 120) {
+            quoteText.classList.add('long-quote');
+        } else {
+            quoteText.classList.remove('long-quote');
+        }
+        // dynamically change the text within specific parts of the Dom
+        quoteText.innerText = data.quoteText;
+        // Stop loader and show quote
+        removeLoadingSpinner()
+    } catch (error) {
+        getQuoteFromApi();
+    }
+}
+
+// Tweet quote
+function tweetQuote() {
+    const quote = quoteText.innerText;
+    const author = authorText.innerText;
+    const twitterUrl = `https://twitter.com/intent/tweet?text=${quote} - ${author}`;
+    window.open(twitterUrl, '_blank');
+}
+// Event listeners
+newQuoteBtn.addEventListener('click', getQuoteFromApi);
+twitterBtn.addEventListener('click', tweetQuote);
+
+
+
+// runs when page is loaded
+getQuoteFromApi();
+// loading();
